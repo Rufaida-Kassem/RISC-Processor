639b2f203174ca00484f17022cc40add3c5f8a83
rufaida-kassem <rufaidahkassem@gmail.com>
2022-11-15 15:57:50 +0200

    id init

--- phase_1/verilog/ID.v
+++ phase_1/verilog/ID.v
@@ -1,5 +1,5 @@
 module ID #(parameter width = 16) (
-    clk, instruction, read_enable, read_addr1, read_addr2, write_addr, MemR, MemWR, aluSrc, RegWR, aluOp
+    clk, instruction, MemR, MemWR, aluSrc, RegWR, aluOp, Op2Immid
   );
   input clk;
   input [width - 1 : 0] instruction;
@@ -7,8 +7,13 @@ module ID #(parameter width = 16) (
   output [5 - 1 : 0] read_addr1, read_addr2;
   output [5 - 1 : 0] write_addr;
   output wire [2:0] aluOp, opCode;
-  output wire RegWR, aluSrc, MemWR, MemR ;
+  output wire RegWR, MemWR, MemR ;
+  output wire aluSrc;
+  output wire Op2Immid;
+  reg fetchOp2 = 'b0;
   reg [2:0]opCode;
+
+  assign Op2Immid = instruction;
   RegFile 
   RegFile_dut (
     .read_enable (read_enable ),
@@ -36,12 +41,12 @@ module ID #(parameter width = 16) (
   always @(posedge clk)
   begin
     //TODO: decode the op code
-    opCode <= instruction  [width - 1 : width - 3];
-    
-
-    read_addr1 <= instruction[25 : 21];
-    read_addr2 <= instruction[20 : 16];
+    if(fetchOp2 == 1'b0)
+    begin opCode <= instruction  [width - 1 : width - 5];
+    read_addr1 <= instruction[width - 6 : width - 8];
+    read_addr2 <= instruction[width - 9 : width - 11];
     read_enable <= 'b1;
-    write_addr <= instruction[15 : 11];
+    write_addr <= instruction[width - 12 : width - 14];
+    end 
   end
 endmodule
