0f7545bbf47ab4fe990ad12443d81a823b7c2d3b
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2022-11-22 17:12:47 +0200

    modify it to be suitable for both data and instruction memories

--- phase_1/verilog/Cache.v
+++ phase_1/verilog/Cache.v
@@ -1,7 +1,8 @@
 
 module Memory #(parameter addBusWidth = 32, width = 16) (
-    clk, rst, memR, memWR, addR, addWR, dataWR, dataR
+    clk, rst, memR, memWR, addR, addWR, dataWR, dataR, instr_data  // instr = 1  data = 0
 );
+    input instr_data;
     input clk, rst, memR, memWR;
     input [width - 1 : 0] dataWR;
     input [addBusWidth - 1 : 0] addR, addWR;
@@ -12,7 +13,12 @@ module Memory #(parameter addBusWidth = 32, width = 16) (
     always @(posedge clk , posedge rst) begin
         if(rst)
         begin
-            $readmemh("mem.mem", memory);
+            if(instr_data) begin
+                $readmemh("mem.mem", memory);
+            end
+            else begin
+                $readmemh("data.mem", memory);
+            end
         end
         else if (clk & memWR) begin
             memory[addWR] <= dataWR;
