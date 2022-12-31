50c81aea42da7260867ad93dbe4d4afdc16b964c
Doaa Magdy <71094316+doaamagdy2024@users.noreply.github.com>
2022-12-31 16:31:52 +0200

    modify opCodes

--- Verilog Files/ALU.v
+++ Verilog Files/ALU.v
@@ -11,20 +11,21 @@ module ALU(input [15:0]Rs,
  Decoder deco(AluOp,DecOut);
 ////////////////////////////////////ONE OPERANDS MODULE//////////////////////////
 //0-NOP
-//1-Not
- Not not_ins(.Rds(Rd),.Enable(DecOut[1]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
-//2-SETC
-SETC setc_ins(.Enable(DecOut[2]),.previousflags(Ccr),.Ccr(Ccr));
-//3-CLRC
-CLRC clrc_ins(.Enable(DecOut[3]),.previousflags(Ccr),.Ccr(Ccr));
+
+//1-SETC
+SETC setc_ins(.Enable(DecOut[1]),.previousflags(Ccr),.Ccr(Ccr));
+//2-CLRC
+CLRC clrc_ins(.Enable(DecOut[2]),.previousflags(Ccr),.Ccr(Ccr));
+//3-Not
+ Not not_ins(.Rds(Rd),.Enable(DecOut[3]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
 //4-INC
 INC inc_ins(.Rds(Rd),.Enable(DecOut[4]),.Out(Out),.previousflags(Ccr),.Ccr(Ccr));
 //5-DEC
 DEC dec_ins(.Rds(Rd),.Enable(DecOut[5]),.Out(Out),.previousflags(Ccr),.Ccr(Ccr));
 //6-OUT
 OUT out_ins(.Rds(Rd),.Enable(DecOut[6]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
-//7-IN
-IN in_ins(.Rds(inport),.Enable(DecOut[7]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
+//Push 7
+PUSH push_ins(.Rds(Rd),.Enable(DecOut[7]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
 
 ////////////////////////////////////TWO OPERANDS MODULE//////////////////////////
 Mov movOp(.Rs(Rs),.Enable(DecOut[8]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
@@ -42,8 +43,10 @@ ShiftRight rightOp(.Rs(Rs),.shiftAmmount(Rd),.Enable(DecOut[14]),.previousflags(
 
 
 ////////////////////////////////////MEMORY OPERATION MODULE//////////////////////////
-//Push 15
-PUSH push_ins(.Rds(Rd),.Enable(DecOut[15]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
+
+
+//15-IN ///////////////////////////
+IN in_ins(.Rds(inport),.Enable(DecOut[15]),.previousflags(Ccr),.Out(Out),.Ccr(Ccr));
 //Pop 16
 // POP pop_ins(.Rds(Rd),.Enable(DecOut[16]),.Out(Out));
 //Ldm 17
