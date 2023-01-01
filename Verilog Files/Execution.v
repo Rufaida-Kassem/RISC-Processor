e135eac44179a33f22154fa1b4b2aeab4289bb17
emanshahda <eman.shahda01@eng-st.cu.edu.eg>
2023-01-01 21:22:12 +0200

    ..

--- Verilog Files/Execution.v
+++ Verilog Files/Execution.v
@@ -1,4 +1,5 @@
-module Execution(input [15:0]op1,
+module Execution(input rst,
+                 input [15:0]op1,
                  input [15:0]op2,
                  input[15:0] inport,
                  input[15:0]immediate,
@@ -11,12 +12,20 @@ module Execution(input [15:0]op1,
                  input [15:0] MemoryWBForwarding,
                  input[1:0] Forward1Sel,
                  input[1:0] Forward2Sel,
-                 inout[2:0] Ccr,
-                 output[15:0]MemoryAddress,
-                 output[15:0] Out
+                 inout [2:0] Ccr,
+                 output [15:0]MemoryAddress,
+                 output [15:0] Out
                  );
 wire [15:0] InOp1Sel,Op1,InOp2Sel,Op2;
 wire signalJump;
+
+// always @(posedge rst) begin
+// if(rst) begin
+// Out = 16'b0;
+// MemoryAddress = 16'b0;
+// end
+// end
+// setCcrValue setCCR(.rst(rst),.Ccr(Ccr));
 //////////////OPERAND 1//////////////////////////////////////
 Mux16Bit muxRd(.a(op1),.b(inport),.sel(Inport),.out(InOp1Sel)); //Selection between inport and src 1
 Mux3x1  muxOp1(.a(InOp1Sel),.b(ExecuteMemoryForwarding),.c(MemoryWBForwarding),.sel(Forward1Sel),.out(Op1)); //Selected op1 of Alu
@@ -24,6 +33,6 @@ Mux3x1  muxOp1(.a(InOp1Sel),.b(ExecuteMemoryForwarding),.c(MemoryWBForwarding),.
 Mux3x1  muxForwardSel(.a(op2),.b(ExecuteMemoryForwarding),.c(MemoryWBForwarding),.sel(Forward2Sel),.out(InOp2Sel)); //Selected between op2 and forwarding
 Mux3x1  muxOp2(.a(InOp2Sel),.b(immediate),.c(shiftAmmount),.sel(AluScr),.out(Op2));//Selected op2 of Alu
 
- ALU alu(Op1,Op2,AluOp,inport,Ccr, Out,signalJump);
+ALU alu(Op1,Op2,AluOp,inport,Ccr, Out,signalJump);
 assign MemoryAddress=Op2;
 endmodule
