43ecdbb23ff37b97db2ef85851bf33370ff7b918
rufaida-kassem <rufaidahkassem@gmail.com>
2023-01-02 01:51:11 +0200

    Merge branch 'Phase-3' of https://github.com/Rufaida-Kassem/RISC-Processor into Phase-3

--- Verilog Files/Processor.v
+++ Verilog Files/Processor.v
@@ -100,6 +100,8 @@ module Processor (
   wire [1:0]Forward1Sel,Forward2Sel;
   wire fetch_pc_enable_oring;
   wire ldm_value;
+  wire flush;
+
 
   assign fetch_pc_enable_oring = (rst == 1'b1) ? 1'b1 : fetch_pc_enable;
 
@@ -156,7 +158,8 @@ module Processor (
       .RW_Sig_in(MEMOWB_Reg[37]),
       .portR(portR),
       .portWR(portWR),
-      .src_address(src_address)
+      .src_address(src_address),
+      .flush(flush)
     );
   
 
@@ -251,7 +254,7 @@ always @ (posedge clk, posedge rst)
         IFIDReg  = {16'b0, pc, instruction};  
         Reg_data_2 = Reg_data;
       end
-      else// if(clk)    //and ~aluSrc_sig
+      else if(~flush)    //and ~aluSrc_sig
       begin
         MEMOWB_Reg ={EXMEMO_Reg[38],EXMEMO_Reg[78:63],Out_Memo, EXMEMO_Reg[50],EXMEMO_Reg[37], EXMEMO_Reg[41:39]};
         EXMEMO_Reg ={Ccr,Out_Excute,MemoryAddress[11:0],
@@ -268,6 +271,12 @@ always @ (posedge clk, posedge rst)
         IFIDReg  = {16'b0, pc, instruction};  
         Reg_data_2 = Reg_data;
       end
+      else
+        begin
+          IFIDReg = 'b0;
+          IDEReg = 'b0;
+          IDEPC_Reg = 'b0;
+        end
     end
 
 
